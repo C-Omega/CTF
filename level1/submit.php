4025c2a48bce3aec9fcf6c65b1a698e48587273c
Cyclic3 <harlan.connor@gmail.com>
2017-01-25 11:58:26 +0000

    debug

--- level1/submit.php
+++ level1/submit.php
@@ -5,14 +5,5 @@ ini_set('display_errors', 1);
 $key = rand();
 $val = $_POST["key"];
 echo "You typed: $val";
-/*if ($key === $val)
-{
-    echo YAY!;
-}
-else
-{
-    echo NO!;
-}
-echo "done"
-*/
+echo ($key === $val) ? "YAY!" : "NO!"
 ?>
