ee683387d581c1d772e810657df2f4f9796970da
Cyclic3 <harlan.connor@gmail.com>
2017-01-25 10:57:20 +0000

    PHP hax

--- level0/index.php
+++ level0/index.php
@@ -1,5 +1,21 @@
 Secret key! <input id="key"> <button id="sub">Submit</button>
 <?php
 $key = rand();
-echo '<script>$(document).ready(function(){$("#sub").click(function(){if ($("#key").val() == ' . $key . '){alert("Correct");} else {alert("Wrong"); });});';
+echo '
+<script>
+	$(document).ready(function()
+	{
+		$("#sub").click(function()
+		{
+			if ($("#key").val() == ' . $key . '))
+			{
+				alert("Correct");
+			}
+			else
+			{
+				alert("Wrong");
+			}
+		});
+	});
+</script>';
 ?>
